a29455cb1caa619fdd6eba89f5365befec1bffa8
Janett Ramirez <jramirezsevilla@pugetsound.edu>
2021-11-13 16:25:13 -0800

    Added eventListeners to the red, yellow, and green buttons to change the border accordingling.

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
+//Changes the border of the blue button accordingly
 let blue = document.querySelector("#blueSq");
 blue.addEventListener("mouseover", () => {
     document.querySelector("#blueSq").classList.add("hover");
 });
+
 blue.addEventListener("mouseout", () => {
     document.querySelector("#blueSq").classList.remove("hover");
+});
+
+//Changes the border of the red button accordingly
+let red = document.querySelector("#redSq");
+red.addEventListener("mouseover", () => {
+    document.querySelector("#redSq").classList.add("hover");
+});
+red.addEventListener("mouseout", () => {
+    document.querySelector("#redSq").classList.remove("hover");
+});
+
+//Changes the border of the yellow button accordingly
+let yellow = document.querySelector("#yellowSq");
+yellow.addEventListener("mouseover", () => {
+    document.querySelector("#yellowSq").classList.add("hover");
+});
+yellow.addEventListener("mouseout", () => {
+    document.querySelector("#yellowSq").classList.remove("hover");
+});
+
+//Changes the border of the green button accordingly
+let green = document.querySelector("#greenSq");
+green.addEventListener("mouseover", () => {
+    document.querySelector("#greenSq").classList.add("hover");
+});
+green.addEventListener("mouseout", () => {
+    document.querySelector("#greenSq").classList.remove("hover");
 });
