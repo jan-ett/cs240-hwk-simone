3973862a7a3bf9ab93faf11ac9b8849954e51983
Janett Ramirez <jramirezsevilla@pugetsound.edu>
2021-11-13 21:57:24 -0800

    Added eventListeners to light up each button accordingly when clicked and play the corresponding sound as well.

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ blue.addEventListener("mouseout", () => {
     document.querySelector("#status").innerHTML = "";
 });
 
+//Causes the blue button to light up and play the corresponding sound when clicked
+blue.addEventListener("mousedown", () => {
+    document.querySelector("#blueSq").classList.add("lightblue");
+});
+blue.addEventListener("mouseup", () => {
+    document.querySelector("#blueSq").classList.remove("lightblue");
+    let audio = new Audio("sounds/blue.wav");
+    audio.play();
+});
+
 //Changes the border of the red button accordingly
 let red = document.querySelector("#redSq");
 red.addEventListener("mouseover", () => {
@@ -19,6 +29,16 @@ red.addEventListener("mouseover", () => {
 });
 red.addEventListener("mouseout", () => {
     document.querySelector("#redSq").classList.remove("hover");
+});
+
+//Causes the red button to light up and play the corresponding sound when clicked
+red.addEventListener("mousedown", () => {
+    document.querySelector("#redSq").classList.add("lightred");
+});
+red.addEventListener("mouseup", () => {
+    document.querySelector("#redSq").classList.remove("lightred");
+    let audio = new Audio("sounds/red.wav");
+    audio.play();
 });
 
 //Changes the border of the yellow button accordingly
@@ -30,6 +50,16 @@ yellow.addEventListener("mouseout", () => {
     document.querySelector("#yellowSq").classList.remove("hover");
 });
 
+//Causes the yellow button to light up and play the corresponding sound when clicked
+yellow.addEventListener("mousedown", () => {
+    document.querySelector("#yellowSq").classList.add("lightyellow");
+});
+yellow.addEventListener("mouseup", () => {
+    document.querySelector("#yellowSq").classList.remove("lightyellow");
+    let audio = new Audio("sounds/yellow.wav");
+    audio.play();
+});
+
 //Changes the border of the green button accordingly
 let green = document.querySelector("#greenSq");
 green.addEventListener("mouseover", () => {
@@ -37,6 +67,16 @@ green.addEventListener("mouseover", () => {
 });
 green.addEventListener("mouseout", () => {
     document.querySelector("#greenSq").classList.remove("hover");
+});
+
+//Causes the green button to light up and play the corresponding sound when clicked
+green.addEventListener("mousedown", () => {
+    document.querySelector("#greenSq").classList.add("lightgreen");
+});
+green.addEventListener("mouseup", () => {
+    document.querySelector("#greenSq").classList.remove("lightgreen");
+    let audio = new Audio("sounds/green.wav");
+    audio.play();
 });
 
 
